d42b098f29a8a1e3719797a2eaec7ae1b2aec7d4
Beshir Saeed <Beshir-saeed@live.com>
2023-02-21 23:47:50 +0300

    Add phone screen

--- src/App.jsx
+++ src/App.jsx
@@ -7,42 +7,39 @@ function App() {
 
 	return (
 		<div className="App">
-			<div className="bg-gray-600 flex flex-col items-center gap-6">
-				<header className="flex p-6 bg-gray-800 h-34 w-screen">
-					<img className="rounded-md w-12 h-12 mr-6" src="/icon.png" alt="" srcset="" />
-					<h2 className="text-3xl font-bold text-white"> Ana Ninja</h2>
-				</header>
-				<div className="flex flex-col items-center ">
-					<img className="rounded-2xl w-24 h-24 m-12" src="/icon.png" />
-					<h1 className="text-3xl font-bold text-white"> Ana Ninja</h1>
-				</div>
-				<div className="flex flex-col items-center gap-6">
-					<span className="text-2xl font-bold text-white">
-						{" "}
-						Grocery delivery for DOPEs{" "}
-					</span>
-					<div className="flex gap-2">
-						<img
-							className="rounded-md w-48 mr-4"
-							src="/playstore.png"
-							alt="playstore"
-						/>
-						<img className="rounded-md w-48 mr-4" src="/appstore.png" alt="playstore" />
+			<div className=" flex flex-col items-center gap-14">
+				<div className="bg-gray-600 pb-36 rounded-b-3xl">
+					<header className="flex items-center px-6 py-8 bg-gray-800 h-34 w-screen">
+						<img className="rounded-lg w-12 mr-4" src="/icon.png" alt="" srcset="" />
+						<h2 className="text-xl font-bold text-white"> Ana Ninja</h2>
+					</header>
+					<div className="flex flex-col items-center ">
+						<img className="rounded-3xl w-28 mt-3 mb-4" src="/icon.png" />
+						<h1 className="text-3xl font-bold text-white"> Ana Ninja</h1>
+					</div>
+					<div className="flex flex-col items-center gap-12">
+						<span className="text-2xl font-bold text-white">
+							{" "}
+							Grocery delivery for DOPEs{" "}
+						</span>
+						<div className="flex gap-2">
+							<img
+								className="rounded-md w-56 mr-4"
+								src="/playstore.png"
+								alt="playstore"
+							/>
+							<img
+								className="rounded-md w-56 mr-4"
+								src="/appstore.png"
+								alt="playstore"
+							/>
+						</div>
 					</div>
 				</div>
-
-				<img
-					className="rounded-md mr-4 max-w-xs"
-					src="/black-iphone.png"
-					alt=""
-					srcset=""
-				/>
+				<div className="rounded-md w-64 black-iphone flex justify-center items-center">
+					<img className="rounded-3xl w-56" src="/PhoneScreen.png" alt="" srcset="" />
+				</div>
 			</div>
-
-			{/* <main>
-				
-				
-			</main> */}
 		</div>
 	);
 }
