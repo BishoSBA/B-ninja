e3b1a00561f3deae579ac19e300b60e74728887c
Beshir Saeed <Beshir-saeed@live.com>
2023-02-23 12:24:34 +0300

    Add responsiveness to smaller screens

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<div className="App">
-			<div className=" flex flex-col items-center gap-14">
-				<div className="flex flex-col bg-gray-600 pb-36 rounded-b-3xl">
-					<header className="flex items-center px-6 py-8 bg-gray-800 h-34 w-screen">
-						<img className="rounded-lg w-12 mr-4" src="/icon.png" alt="" srcset="" />
+			<div className=" items-center gap-8">
+				<div className="flex flex-col h-screen rounded-b-3xl gap-12 background-Image">
+					<header className="flex items-center px-6 py-8 gap-4 bg-white bg-opacity-10 h-34">
+						<img className="rounded-lg w-12" src="/icon.png" alt="" srcset="" />
 						<h2 className="text-xl font-bold text-white"> Ana Ninja</h2>
 					</header>
-					<div className="flex flex-col items-center ">
-						<img className="rounded-3xl w-28 mt-3 mb-4" src="/icon.png" />
+
+					<div className="flex flex-col items-center md:flex-row justify-center gap-6">
+						<img className="rounded-3xl w-28 mt-3" src="/icon.png" />
 						<h1 className="text-3xl font-bold text-white"> Ana Ninja</h1>
 					</div>
 					<div className="flex flex-col items-center gap-12">
@@ -25,24 +24,24 @@ function App() {
 							Grocery delivery for DOPEs{" "}
 						</span>
 
-						<div className="flex gap-2">
+						<div className="flex flex-col gap-2 sm:flex-row">
 							<img
-								className="rounded-md w-56 mr-4"
+								className="rounded-md w-64 mr-4"
 								src="/playstore.png"
 								alt="playstore"
 							/>
 							<img
-								className="rounded-md w-56 mr-4"
+								className="rounded-md w-64 mr-4"
 								src="/appstore.png"
 								alt="playstore"
 							/>
 						</div>
 					</div>
 				</div>
-				<div className="rounded-md w-64 black-iphone flex justify-center items-center">
+				<div className="rounded-md w-64 m-auto flex items-center justify-center black-iphone -mt-16 sm:-mt-32">
 					<img className="rounded-3xl w-56" src="/PhoneScreen.png" alt="" srcset="" />
 				</div>
-				<section className="pb-12 -mt-12">
+				<section className="pb-12 lg:pt-12">
 					<ul className="flex flex-col px-10 gap-8 lg:flex-row lg:gap-0 max-w-6xl">
 						<li className="flex text-xl gap-6 lg:gap-0">
 							<div>
@@ -59,7 +58,7 @@ function App() {
 								</p>
 							</div>
 						</li>
-						<li className="flex px-10 text-xl gap-6 ">
+						<li className="flex text-xl gap-6 ">
 							<div>
 								<span className="fa-stack fa-1x ">
 									<i className=" text-gray-200 fas fa-circle fa-stack-2x"></i>
@@ -67,14 +66,14 @@ function App() {
 								</span>
 							</div>
 							<div>
-								<h3 className=" text-2xl font-bold mb-3">Supermarket prices</h3>
+								<h3 className=" text-2xl font-bold mb-3">Ultra-fast delivery</h3>
 								<p className=" text-2lg">
-									Over 2000 items, direct to your door, at normal supermarket
-									prices!
+									Our riders bring your groceries, direct to your door, in
+									minutes.
 								</p>
 							</div>
 						</li>
-						<li className="flex px-10 text-xl gap-6 ">
+						<li className="flex text-xl gap-6 ">
 							<div>
 								<span className="fa-stack fa-1x ">
 									<i className=" text-gray-200 fas fa-circle fa-stack-2x"></i>
@@ -82,16 +81,24 @@ function App() {
 								</span>
 							</div>
 							<div>
-								<h3 className=" text-2xl font-bold mb-3">Supermarket prices</h3>
+								<h3 className=" text-2xl font-bold mb-3">Fresh & Organic</h3>
 								<p className=" text-2lg">
-									Over 2000 items, direct to your door, at normal supermarket
-									prices!
+									Choose from our large selection of fresh and organic items.
 								</p>
 							</div>
 						</li>
 					</ul>
 				</section>
 			</div>
+			<footer>
+				<p className="text-center pb-8 text-xl text-gray-500">
+					© 2022 Ninja, All rights reserved
+					<br />
+					شركة تقنية التقدم لتقنية المعلومات
+					<br />
+					ADVANCED TECHNOLOGIES COMPANY FOR INFORMATION TECHNOLOGY
+				</p>
+			</footer>
 		</div>
 	);
 }
